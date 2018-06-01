49b4b2dcdceef922df83796ea5a514e973ba8203
Ariel Zhen <zhenpingping@live.com>
2018-06-01 04:56:30 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,15 @@ class String
   end
 
   def count_sentences
-
-  end
+    array = self.split(/\W+/) 
+    counter = 0 
+    while counter <= array.length 
+      array.each do |x|
+        if sentence?(x) || question?(x) || exclamation?(x) && x.length > 1 
+          counter += 1 
+        end 
+      end 
+    
+      counter 
+    end
 end
